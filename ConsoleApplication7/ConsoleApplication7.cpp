a78680a046bea44c2d1b03d8d221a6e59e685def
RomanLyashenko <159914055+RomanLyashenko@users.noreply.github.com>
2024-12-05 21:17:51 +0300

    Add files via upload

---
+++ ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+#include "List.h"
+#include "Queue.h"
+
+int main()
+{
+    List<int> obj;
+    for (int i = 0; i < 10; i++) {
+        obj.pushBack(i);
+    }
+
+}
